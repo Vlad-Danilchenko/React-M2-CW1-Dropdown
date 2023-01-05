eb67f3553c16664e3c4085b66c56898ebefa46ce
Freasy-Vlad <vladdanilchenkodune@gmail.com>
2023-01-05 18:30:09 +0200

    add start files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Dropdown from 'components/Dropdown';
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <h1>Состояние компонента</h1>
+
+      <Dropdown />
+    </>
+  );
+};
+
+export default App;
